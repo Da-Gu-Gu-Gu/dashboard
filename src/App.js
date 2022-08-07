b243223182f46a6a565d94b2bf3819a8fc231203
tankamout <daguguguu@gmail.com>
2022-08-07 20:16:06 +0630

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routers>
+      <div className="bg-red-300 flex w-screen min-h-screen">
+        <div className="sidebar bg-red-600 w-[200px] h-screen">Sidebar</div>
+        <div className="grow flex flex-col">
+          <div className="nav h-[50px]  bg-blue-300  w-full">aa</div>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Routers>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" h-full flex items-center bg-white justify-center">
+      HOme
+    </div>
+  );
+};
+
+export default Home;
